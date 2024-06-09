0de7e31e5bf0d72db456e303373b1accde92d548
Ganesh7386 <puttaganesh7386@gmail.com>
2024-06-09 14:35:06 +0530

    removed bug in Chat interface

--- frontend/src/components/ChatSec.jsx
+++ frontend/src/components/ChatSec.jsx
@@ -16,7 +16,7 @@ const ChatSec = () => {
                 <div className=' w-[32%] pt-[2vw] text-[1.2vw] tracking-tight font-martian ' >
                     <p>Feeling stressed? Overwhelmed? Meet our virtual counsellor, it’s here to chat 24/7. Simply pour your heart out, it won’t interrupt, judge or get tired.</p>
                 </div>
-                <button className=' mt-[2vw] p-[.6vw] rounded-xl bg-black text-[#F9C74F] border-4 ' onClick={handleStartChatting} >Start Chatting</button>
+                <button className=' mt-[2vw] p-[.6vw] rounded-xl bg-black text-[#F9C74F] border-4 w-[160px] h-[70px]' onClick={handleStartChatting} >Start Chatting</button>
             </div>
         </div>
     </div>
